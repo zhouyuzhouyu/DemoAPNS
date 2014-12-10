962d74f35e7153d773f1f3995956ba5fdacbb65e
zoyu <524112470@qq.com>
2014-12-10 16:03:18 +0800

    init

--- DemoAPNS/DemoPush/DemoPush/ApnsManager.h
+++ DemoAPNS/DemoPush/DemoPush/ApnsManager.h
@@ -1,31 +1,24 @@
 //
 //  ApnsManager.h
-//  hbgj
 //
-//  Created by 张 林 on 12-8-1.
-//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+//  Created by zoyu on 14-12-10.
+//  Copyright (c) 2012年 hcdl. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 #define kDefault_Apn_Token @"1111"
-@class UIApnsAlertView;
 
-@interface ApnsManager : NSObject <UIAlertViewDelegate> {
-    NSString *_token;
-    UIApnsAlertView *avApns;
-    NSDate *dateRefresh;
-}
+@interface ApnsManager : NSObject
 
-@property (nonatomic,retain) NSString *token;
-@property (nonatomic,retain) UIApnsAlertView *avApns;
-@property (nonatomic,retain) NSDate *dateRefresh;
+@property (nonatomic,strong) NSString *token;
+@property (nonatomic,strong) NSDate *dateRefresh;
 
 + (id)shared;
 
 - (void)refresh; //刷新apns
-- (void)registerForRemoteNotification;
 
 //处理启动是由APNS启动
 - (BOOL)handleLaunchOptionsRemoteNotification:(NSDictionary *)launchOptions;
@@ -48,19 +41,4 @@
 
 
 
-
-
-//-------------------------------------------------------------------------------------------------------------
-//
-// UIApnsAlertView
-// Apns消息弹框
-//--------------------------------------------------------------------------------------------------------------
-@interface UIApnsAlertView : UIAlertView {
-	NSDictionary *dicApns;
-	int nType; //1:航班订阅 (确定)  2:航班订阅(关闭,显示) 3：酒店订阅（确定） 4：酒店订阅（确定，显示）
-}
-@property (nonatomic, retain) NSDictionary *dicApns;
-@property (nonatomic, assign) int nType;
-
-@end
 
--- DemoAPNS/DemoPush/DemoPush/ApnsManager.m
+++ DemoAPNS/DemoPush/DemoPush/ApnsManager.m
@@ -1,9 +1,9 @@
 //
 //  ApnsManager.m
-//  hbgj
+//  
 //
-//  Created by 张 林 on 12-8-1.
-//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//  Created by zoyu on 14-12-10.
+//  Copyright (c) 2014年 hcdl. All rights reserved.
 //
 
 #import "ApnsManager.h"
@@ -14,9 +14,6 @@
 static ApnsManager *sharedApnsManager = nil;
 
 @implementation ApnsManager
-@synthesize token = _token;
-@synthesize avApns;
-@synthesize dateRefresh;
 
 + (id)shared {
 	@synchronized(self)
@@ -36,7 +33,7 @@ static ApnsManager *sharedApnsManager = nil;
     if (self !=nil ) {
         self.token = [[NSUserDefaults standardUserDefaults] stringForKey:kUserDefaults_Key_APNSToken];
         NSLog(@"token = %@", _token);
-        if (_token.length==0 ) {
+        if (_token.length == 0 ) {
             self.token = kDefault_Apn_Token;
         }
         [self refresh];
@@ -45,8 +42,8 @@ static ApnsManager *sharedApnsManager = nil;
 }
 
 - (void)refresh {
-    if(dateRefresh!=nil) {
-        NSTimeInterval tval = [dateRefresh timeIntervalSinceNow];
+    if(self.dateRefresh!=nil) {
+        NSTimeInterval tval = [self.dateRefresh timeIntervalSinceNow];
         if ( tval > -600.0 ) {
             return ;
         }
@@ -64,84 +61,32 @@ static ApnsManager *sharedApnsManager = nil;
 
 //处理收到注册成功的消息
 - (void)handleRegisterForRemoteNotificationsWithDeviceToken:(NSData *)devToken {
-#if 1
-    const char *devTokenBytes = [devToken bytes];
-	int len = [devToken length];
-	char temps[1024];
-	temps[0] = '\0';
-	for ( int i=0; i<len; i++ ) {        
-		char temps2[100];
-		sprintf(temps2, "%02x", (unsigned char) devTokenBytes[i] );
-		strcat( temps, temps2 );
-	}
 
-	NSString *strToken = [NSString stringWithCString:temps encoding:NSASCIIStringEncoding];
-	NSLog(@"token = %@",strToken);
-#endif
-//    NSString *strToken = [NSString stringWithFormat:@"%@",devToken];
-//    NSLog(@"deviceToken:%@", strToken);
-//    strToken = [strToken stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]]; //去掉"<>"
-//    strToken = [strToken stringByReplacingOccurrencesOfString:@" " withString:@""];//去掉中间空格
-//    NSLog(@"deviceToken:%@", strToken);
-	
+    NSString *strToken = [NSString stringWithFormat:@"%@",devToken];
+    strToken = [strToken stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]]; //去掉"<>"
+    strToken = [strToken stringByReplacingOccurrencesOfString:@" " withString:@""];//去掉中间空格
+	NSLog(@"deviceToken:%@", strToken);
 	//新旧token不一样才上传
 	if( ![strToken isEqualToString:self.token] )
 	{
-//        [[AppSetting shared] settingSetString:strToken forKey:@"apntoken"];
         [[NSUserDefaults standardUserDefaults] setObject:strToken forKey:kUserDefaults_Key_APNSToken];
         self.token = strToken;
-#if 0
-		//上传设备信息
-        NSString *stampId = [theApp stampId];
-        if (ZYIsNullOrEmpty(stampId)) {
-            ZYSocketRequest *request = [ZYSocketRequest getMachineStampID];
-            [ZYSocket socketRequset:request success:^(ZYSocketResponse *socketResponse) {
-                NSDictionary *dic = [socketResponse.arrRowData objectAtIndex:0];
-                NSString *stampId = [dic objectForKey:@"v_stamp_id"];
-                [theApp saveStampId:stampId];
-                
-                ZYSocketRequest *request = [ZYSocketRequest bindTokenRequest:strToken stamp:[theApp stampId]];
-                [ZYSocket socketRequset:request success:^(ZYSocketResponse *socketResponse) {
-                    CJLog(@"%s\n 绑定token和stampId 成功11",__FUNCTION__);
-                } failure:^(ZYSocketResponse *socketResponse) {
-                    CJLog(@"%s\n 上传token到服务器失败11",__FUNCTION__);
-                }];
-            } failure:^(ZYSocketResponse *socketResponse) {
-                
-            }];
-        }
-        else
-        {
-            ZYSocketRequest *request = [ZYSocketRequest bindTokenRequest:strToken stamp:[theApp stampId]];
-            [ZYSocket socketRequset:request success:^(ZYSocketResponse *socketResponse) {
-                CJLog(@"%s\n 上传token和stampId 成功12",__FUNCTION__);
-            } failure:^(ZYSocketResponse *socketResponse) {
-                CJLog(@"%s\n 上传token和stampId 到服务器失败12",__FUNCTION__);
-            }];
-        }
-    #endif
 	}
-
 }
 
 //处理收到注册失败的消息
 - (void)handleFailToRegisterForRemoteNotificationsWithError:(NSError *)err {
     NSLog(@">>>FailToRegisterForRemoteNotifications: %@", err);
-//    [[NSNotificationCenter defaultCenter] postNotificationName:k_system_taken_register_failure object:nil];
 }
 
 - (BOOL)handleLaunchOptionsRemoteNotification:(NSDictionary *)launchOptions {
     [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
     NSDictionary *dicApns = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
-//    dicGetDic(launchOptions, UIApplicationLaunchOptionsRemoteNotificationKey);
     if ( dicApns ) {
 #if 1
         NSString *strMessage = [[dicApns objectForKey:@"aps"] objectForKey:@"alert"];
-//        dicGetString( dicGetDic(dicApns, @"aps"), @"alert" );
-
         UIAlertView *av = [[UIAlertView alloc] initWithTitle:@"启动时收到APNS通知" message:strMessage delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil];
         [av show];
-//        [av release];
 #endif
 
         return YES;
@@ -153,25 +98,17 @@ static ApnsManager *sharedApnsManager = nil;
 - (void)handleReceiveRemoteNotification:(NSDictionary *)userInfo {
     NSLog(@"notification body = %@", userInfo);
 	
-	if ( avApns!=nil ) {
-		//[avApns dismissWithClickedButtonIndex:-1 animated:YES];
-		self.avApns = nil;
-	}
-	
 	NSDictionary *dicApns = userInfo;
 	NSString *strMessage = [[dicApns objectForKey:@"aps"] objectForKey:@"alert"];
 //    dicGetString( dicGetDic(dicApns, @"aps"), @"alert" );
 	
     NSLog(@"receive:%@", strMessage);
     UIApplication *application = [UIApplication sharedApplication];
-    NSLog(@"applicationState:%ld", application.applicationState);
-    
     if ( application.applicationState == UIApplicationStateActive ) {
 #if 1
         //激活状态下收到通知
         UIAlertView *av = [[UIAlertView alloc] initWithTitle:@"收到APNS通知" message:strMessage delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil];
         [av show];
-//        [av release];
 #endif
     }
     else {
@@ -179,26 +116,7 @@ static ApnsManager *sharedApnsManager = nil;
     }
 }
 
-- (void)dealloc {
-    self.token = nil;
-    self.avApns = nil;
-    self.dateRefresh = nil;
-
-//    [super dealloc];
-}
 
 @end
 
 
-
-@implementation UIApnsAlertView
-@synthesize dicApns;
-@synthesize nType;
-
-- (void)dealloc {
-//	[dicApns release];
-//    [super dealloc];
-}
-
-
-@end
